957fa361b30b0181f431b0963a5b2fd9339832f6
Claude Sortwell <claude.sortwell@gmail.com>
2020-01-09 10:58:53 +1100

    Public files accessable anywhere

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+function darkMode() {
+    $('#navbar').toggleClass('lightMode');
+    $('#mainLogo').toggleClass('lightMode');
+    $('#navOptions').toggleClass('lightMode');
+    $('#darkModeOption').toggleClass('lightMode');
+    $('#projectLink').toggleClass('projectLinkInvert');
+    $('#header').toggleClass('lightMode');
+    $('#icon1').toggleClass('lightIconHover');
+    $('#icon2').toggleClass('lightIconHover');
+    $('#icon3').toggleClass('lightIconHover');
+    $('#navLink').toggleClass('lightModeA');
+    $('#navLink1').toggleClass('lightModeA');
+    $('#navLink2').toggleClass('lightModeA');
+    $('#headerBackground').toggleClass('lightBackground');
+}
